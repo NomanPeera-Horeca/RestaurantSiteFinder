4f3371b922e167e5ed39dd0ba8043d0395724ced
Noman Peera <nomanpeera@Nomans-MacBook-Pro.local>
2026-05-04 16:29:20 -0500

    Add stub files for missing Manus components

---
+++ client/src/lib/horeca-brand.ts
@@ -0,0 +1,9 @@
+export const HORECA = {
+  name: "Horeca Store",
+  url: "https://www.thehorecastore.com",
+  email: "sales@thehorecastore.com",
+  phone: "866.446.7322",
+  tagline: "100,000+ restaurant equipment products",
+  shopUrl: "https://www.thehorecastore.com",
+};
+export default HORECA;
