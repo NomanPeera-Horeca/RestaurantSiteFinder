7f19b3380f17be0e2a682b1cf05f65c0e517ab3d
Noman Peera <nomanpeera@Nomans-MacBook-Pro.local>
2026-05-04 16:36:31 -0500

    Complete HORECA brand object with all properties

--- client/src/lib/horeca-brand.ts
+++ client/src/lib/horeca-brand.ts
@@ -1,9 +1,18 @@
 export const HORECA = {
   name: "Horeca Store",
-  url: "https://www.thehorecastore.com",
+  tagline: "100,000+ restaurant equipment products",
   email: "sales@thehorecastore.com",
   phone: "866.446.7322",
-  tagline: "100,000+ restaurant equipment products",
-  shopUrl: "https://www.thehorecastore.com",
+  phoneHref: "tel:+18664467322",
+  website: "https://www.thehorecastore.com",
+  logo: "https://www.thehorecastore.com/logo.png",
+  icon: "https://www.thehorecastore.com/favicon.ico",
+  links: {
+    cooking: "https://www.thehorecastore.com/commercial-cooking-equipment",
+    equipment: "https://www.thehorecastore.com/restaurant-equipment",
+    categories: "https://www.thehorecastore.com/categories",
+    searchOrder: "https://www.thehorecastore.com/order-tracking",
+  },
 };
+
 export default HORECA;
